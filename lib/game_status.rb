da8dbbb3af493f88e78d107b21740c43b9edfc3e
Kacy Trujillo <kclnto@gmail.com>
2020-09-08 20:52:18 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -62,12 +62,12 @@ def over?(board)
 end
 
 def winner(board)
-  if board[won?(board)[0]] == "X"
-    return "X"
-  elsif board[won?(board)[0]] == "O"
-    return "O"
-  else
-    return nil
+  if over?(board) == TRUE
+    if board[won?(board)[0]] == "X"
+      return "X"
+    elsif board[won?(board)[0]] == "O"
+      return "O"
+    end
   end
 end
   
