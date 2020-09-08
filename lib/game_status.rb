23461c529b32f0936a2ab483fd1580ef1cea835c
Kacy Trujillo <kclnto@gmail.com>
2020-09-08 19:36:19 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -17,4 +17,10 @@ WIN_COMBINATIONS = [
   
 for each win_combination in WIN_COMBINATIONS
   win_index_1 = win_combination[0]
+  win_index_2 = win_combination[1]
+  win_index_3 = win_combination[2]
+  
+  position_1 = board[win_index_1]
+
+
   
