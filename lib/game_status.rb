18858031544de55ac602c1396d03289ee9c2968c
Kacy Trujillo <kclnto@gmail.com>
2020-09-08 17:37:24 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -3,4 +3,83 @@ def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
 
-# Define your WIN_COMBINATIONS constant
+
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,4,8],
+  [2,4,6],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8]
+  ]
+  
+
+X_combo = []
+O_combo = []
+  
+  
+def won?(board)
+  # I want to leave a comment
+  X_combo = []
+  O_combo = []
+  myIndex = 0
+  # puts(board)
+  
+  for moves_combo in board do
+    if moves_combo == "X"
+      X_combo << myIndex
+    elsif moves_combo == "O"
+      O_combo << myIndex
+    end
+    myIndex+=1
+  end
+  
+  # Show the current X_combo
+  puts("Current X_combo")
+  puts(X_combo)
+  
+  
+  for win_combo in WIN_COMBINATIONS do
+    puts("Current win_combo")
+    puts(win_combo)
+    win_count=0
+
+    puts("Checking if X_combo in win_combo")
+    
+    for x_number in X_combo do
+      for win_number in win_combo do
+        if win_number == x_number
+          win_count+=1
+          puts("Current win_count")
+          puts(win_count)
+        end
+      end
+    end
+    
+    if win_count >= 3
+      return TRUE
+    end
+  end
+  
+  # No winning combos detected
+  return FALSE
+end
+
+def full?(board)
+  board.detect do |empty|
+    if empty == " " || empty == nil
+      return FALSE
+    end
+  end
+end
+
+def draw?
+end
+
+def over?
+end
+
+def winner
+end
